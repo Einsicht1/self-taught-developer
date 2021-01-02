975b32f83bb07214f859ac0bf359f7e4765f03c1
hwanil <hikim@koodon.com>
2021-01-02 18:23:00 +0900

    countries spider modified

--- crawling/projects/worldometers/worldometers/spiders/countries.py
+++ crawling/projects/worldometers/worldometers/spiders/countries.py
@@ -3,14 +3,17 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/world-population/population-by-country']
+    allowed_domains = ['www.worldometers.info']
     start_urls = ['http://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        title = response.xpath("//h1/text()").get()
-        countries = response.xpath("//td/a/text()").getall()
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
 
-        yield {
-            'title': title,
-            'countries': countries
-        }
+            # absolute_url = response.urljoin(link)
+            # absolute_url = f"https://www.worldometers.info{link}"
+
+
+            yield response.follow(url=link)
